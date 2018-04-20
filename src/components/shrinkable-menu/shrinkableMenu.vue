aecb03b2514b146756cead0cdca63fed2e00f6c2
Diamondsiron <273699943@qq.com>
2018-04-20 10:00:21 +0800

    style

---
+++ src/components/shrinkable-menu/shrinkableMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div> </div>
+    <ul>
+      <li>
+        <div></div>
+        <ul>
+          <li @click="handleChange()">富文本编辑器</li>
+          <li @click="handleChange()">图片预览编辑</li>
+          <li @click="handleChange()">城市联动</li>
+          <li @click="handleChange()">文件上传</li>
+          
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default{
+  name:'shrinkableMenu',
+  props:{
+
+  },
+  data(){
+    return{
+        list:[]
+    }
+  },
+  methods:{
+    handleChange(name){
+      this.$router.push({
+          name:name
+      });
+    }
+  }
+}
+  
+</script>
+<style>
+
+</style>
---
+++ src/views/main.vue
@@ -0,0 +1,88 @@
+<template>
+ <div class="main">
+   <div class="sidebar">
+       <shrinkableMenu></shrinkableMenu>
+   </div>
+   <div class="header">
+     <headers></headers>
+   </div>
+   <div class="page">
+      <tagsPageOpened></tagsPageOpened>
+      <div class="page-contains">
+         <router-view></router-view>
+      </div>
+     
+   </div>
+   
+  
+  
+
+ </div>
+</template>
+
+<script>
+
+
+import headers from '../components/header/headers.vue';
+import shrinkableMenu from '../components/shrinkable-menu/shrinkableMenu.vue'
+import tagsPageOpened from '../components/tagsPageOpened/tagsPageOpened.vue'
+
+export default {
+  name: 'Mains',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    headers,
+    shrinkableMenu,
+    tagsPageOpened
+    
+
+  },
+  computed:{
+   
+ 
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  },
+  watch:{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sidebar{
+  width: 200px;
+  overflow: hidden;
+  position: fixed;
+  top:0px;
+  left: 0px;
+  height: 100%;
+}
+.header{
+  position: fixed;
+ 
+  height: 100px;
+  left: 200px;
+  top:0;
+}
+.page{
+    position: absolute;
+    top: 100px;
+    left: 200px;
+    min-width: 600px;
+    bottom: 0;
+    overflow: auto;
+}
+.page-contains{
+  padding:10px;
+}
+</style>
