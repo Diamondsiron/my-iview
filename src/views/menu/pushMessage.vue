b4a85505b7eddb921a9a1177c93a876faf1ea635
Diamondsiron <273699943@qq.com>
2018-06-14 10:42:22 +0800

    message

--- src/views/menu/pushMessage.vue
+++ src/views/menu/pushMessage.vue
@@ -13,10 +13,20 @@
     </div> -->
    <Form ref="formValidate" :model="formValidate"  :label-width="80" style="width: 600px">
         <FormItem label="收件人">
-           <Select v-model="formValidate.name" style="width:200px">
-                      <Option v-for="(item,index) in list" :value="item.operator_id" :key="index">{{ item.name }}</Option>
+                  <Select v-model="sendType" style="width:200px" >
+                      <Option  value="角色" >角色</Option>
+                      <Option  value="机构" >机构</Option>
                     </Select>
-           
+                    <Select v-model="formValidate.name" style="width:200px" @on-change="select" v-if="show">
+                      <Option v-for="(item,index) in roleList" :value="item.role_id" :key="index">{{ item.role_name }}</Option>
+                    </Select>
+                    <Select v-model="formValidate.name" style="width:200px" @on-change="select"  v-if="!show">
+                      <Option v-for="(item,index) in orgList" :value="item.org_id" :key="index">{{ item.org_name }}</Option>
+                    </Select>
+                     <CheckboxGroup v-model="list">
+                    <Checkbox   v-for="(item,index) in userList"  :key="index" :label="item.operator_id">{{item.name}}</Checkbox>
+                    </CheckboxGroup>
+                   <!--  {{list}} -->
         </FormItem>
          <FormItem label="消息类型">
             <Select v-model="formValidate.type" style="width:200px">
@@ -57,9 +67,15 @@ export default{
   name:'pushMessage',
   data(){
     return{
+      orgList:[],
+      roleList:[],
+      userList:[],
+      show:true,
       list:[],
+      sendType:"角色",
       modal1:false,
       formValidate:{
+        sendType:"",
         push:"",
         name:"",
         desc:"",
@@ -68,12 +84,41 @@ export default{
       
     }
   },
+  watch:{
+    sendType:function(){
+      if(this.sendType=="角色"){
+        this.show=true
+      }else{
+        this.show = false
+      }
+    }
+  },
   methods:{
     ok(){
 
     },
     cancel(){
 
+    },
+    select(value){
+      let vm = this
+      vm.list=[]
+      //console.log(value)
+      if(value.indexOf("RL")>-1){
+        for(let i=0;i<vm.roleList.length;i++){
+          if(value==vm.roleList[i].role_id){
+            vm.userList = vm.roleList[i].emp_list
+          }
+        }
+        
+      }else{
+        //console.log("org")
+        for(let i=0;i<vm.orgList.length;i++){
+          if(value==vm.orgList[i].org_id){
+            vm.userList = vm.orgList[i].emp_list
+          }
+        }
+      }
     },
     init(){
                 let vm = this;
@@ -90,17 +135,28 @@ export default{
                     }
                   }
                 }
-                axios.post('api/operator',req).then(function(res){ 
-                     //console.log("data",res.data)
-                     vm.list = res.data.jyau_content.jyau_resData[0].oper_list
-                   
+                //emporg/queryRoleOperator  emporg/queryOrgOperator
+                axios.post('api/emporg/queryOrgOperator',req).then(function(res){ 
+                     console.log("data",res.data)
+                    vm.orgList = res.data.jyau_content.jyau_resData[0].orgemp_list
+                  // vm.userList=res.data.jyau_content.jyau_resData[0].orgemp_list[0].emp_list
                     }).catch(function(error){
                         console.log(error)
                     }) 
+                axios.post('api/emporg/queryRoleOperator',req).then(function(res){ 
+                  console.log("data",res.data)
+                 vm.roleList = res.data.jyau_content.jyau_resData[0].roleemp_list
+                
+                }).catch(function(error){
+                    console.log(error)
+                }) 
       },
     send(){
         
         let vm = this
+        let json =`{["OP201805171438586409", "OP201805171726129979"]}`
+        vm.formValidate.name=json
+        
         vm.formValidate.push=JSON.parse(localStorage.getItem("User")).jyau_content.jyau_resData[0].operator_id
       	if ('WebSocket' in window) {
           websocket = new WebSocket(url + vm.formValidate.push + "/" + vm.formValidate.name+"/"+vm.formValidate.type);
@@ -169,6 +225,8 @@ export default{
 }  
 </script>
 <style scoped>
-
+li{
+  list-style: none;
+}
 </style>
 
